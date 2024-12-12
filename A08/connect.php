a45b9eee5ae62e9c98991e6260c464282f0c1ffe
Joseph Maliza <josephmaliza55@gmail.com>
2024-12-12 19:04:41 +0800

    update

--- A08/connect.php
+++ A08/connect.php
@@ -1,15 +1,15 @@
 <?php
   $dbhost = "localhost:3307";
-	$dbuser = "root";
-	$dbpass = "";
-	$db = "corememories";
+		$dbuser = "root";
+		$dbpass = "";
+		$db = "corememories";
 
-	$conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n". $conn -> error);
+		$conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n". $conn -> error);
 
-	if(!$conn)
-	{
-		die("Connection Failed. ". mysqli_connect_error());
-	}
+		if(!$conn)
+		{
+			die("Connection Failed. ". mysqli_connect_error());
+		}
 
   function executeQuery($query){
     $conn = $GLOBALS['conn'];
