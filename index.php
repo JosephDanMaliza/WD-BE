b96a815b14cf568d81781e084fa04d59b9944598
Joseph Maliza <josephmaliza55@gmail.com>
2024-12-12 19:55:58 +0800

    update

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include('A08/connect.php');
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="assets/css/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&display=swap" rel="stylesheet">
   <title>Portfolio: Joseph</title>
